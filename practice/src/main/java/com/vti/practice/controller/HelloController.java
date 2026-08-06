4ade7aedfe557b0d2e659ec026b6fda57b6c9124
vclong2003 <vclong2003@gmail.com>
2026-08-06 16:31:04 +1000

    ok

---
+++ practice/src/main/java/com/vti/practice/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.vti.practice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World!";
+    }
+}
---
+++ practice/src/main/java/com/vti/practice/entity/Department.java
@@ -0,0 +1,36 @@
+package com.vti.practice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long departmentId;
+
+    @Column(length = 100, unique = true)
+    private String departmentName;
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+}
