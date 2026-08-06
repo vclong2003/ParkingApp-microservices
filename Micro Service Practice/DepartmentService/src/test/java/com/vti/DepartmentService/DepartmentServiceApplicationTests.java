a686c7686efbfc4b6d2c52a73d2b5547df177b75
vclong2003 <vclong2003@gmail.com>
2026-08-07 00:08:59 +1000

    ok

---
+++ Micro Service Practice/DepartmentService/src/test/java/com/vti/DepartmentService/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.DepartmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
