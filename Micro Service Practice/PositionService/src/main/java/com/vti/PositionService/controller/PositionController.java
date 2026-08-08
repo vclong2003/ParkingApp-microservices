91308d25fad599d24e54e5a731cc25d78e8d0f22
vclong2003 <vclong2003@gmail.com>
2026-08-08 23:31:04 +1000

    Back up

--- Micro Service Practice/PositionService/src/main/java/com/vti/PositionService/controller/PositionController.java
+++ Micro Service Practice/PositionService/src/main/java/com/vti/PositionService/controller/PositionController.java
@@ -16,7 +16,6 @@ import com.vti.PositionService.service.PositionService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("api/v1/positions")
