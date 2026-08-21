c934364ff96de9f8822cf185048183b0a7d0d4f1
vclong2003 <vclong2003@gmail.com>
2026-08-21 13:50:10 +1000

    Back up

---
+++ Micro Service Practice/AuthService/src/test/java/com/vti/AuthService/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.AuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
