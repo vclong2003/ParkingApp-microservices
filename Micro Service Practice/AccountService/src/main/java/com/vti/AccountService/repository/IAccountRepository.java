a3ebec3a5e3896d44b377150ba8c98e4e4d4418a
vclong2003 <vclong2003@gmail.com>
2026-09-13 15:49:48 +1000

    Re-organize project

--- Micro Service Practice/AccountService/src/main/java/com/vti/AccountService/repository/IAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vti.AccountService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vti.AccountService.entity.Account;
-
-public interface IAccountRepository extends JpaRepository<Account, Short> {
-
-}
